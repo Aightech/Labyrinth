08d02ea64f92eb20a95d1e10b28acbf64c183d65
Aightech <aiiight.tech@gmail.com>
2016-11-21 21:45:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "struct.h"
 
+/*
+int dispMap(Map L);//Display the labyrinth
+
+int move(Map L, Player P,char move);//return 1. If can't move return : -1(wall) -2(player) 
+int moveMap(Map L,int row, int line);//return 1. If can't move return : -1(not enought energy)
+
+Path Astar(Map L,Player P1, Player P2);//return the shortest path beetween P1 and P2 (could work with Map move)
+int evaluate(Map L);
+*/
 int main()
 {
 	return 0;
