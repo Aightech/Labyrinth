f0b635505627c5227c16e61543742a27257ffa8b
ARLANDIS Maeva <3407447@pc4018.polytech.upmc.fr>
2017-01-10 11:27:18 +0100

    comment for doxygen

--- dumbMd.h
+++ dumbMd.h
@@ -3,5 +3,10 @@
 #include "struct.h"
 //#include "labyrinthAPI.h"
 
+/*! \fn void dumbMode(Map* L)
+    \brief Mode of the game where the player control his moves turn by turn with the console.
+    \param L The labyrinth map structure.
+*/
+
 void dumbMode(Map* L);
 #endif
